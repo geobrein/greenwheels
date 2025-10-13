6c3d7d8f3572b8a3f6190b4967965c8321416ac9
geobrein <98982006+geobrein@users.noreply.github.com>
2025-10-13 14:06:23 +0200

    Update script.R

--- script.R
+++ script.R
@@ -49,6 +49,9 @@ res <- httr::POST(
 httr::stop_for_status(res)
 out <- httr::content(res, as = "parsed", type = "application/json")
 
+# Voeg runtime-datum/tijdkolommen toe (Europe/Amsterdam) aan de pipeline
+tz_now <- as.POSIXct(Sys.time(), tz = "Europe/Amsterdam")
+
 # Flatten -> pak 1e (enige) auto per locatie -> unnest naar vlakke kolommen
 locs <- jsonlite::fromJSON(jsonlite::toJSON(out$data$locations, auto_unbox = TRUE), flatten = TRUE) %>%
   dplyr::mutate(
@@ -59,10 +62,13 @@ locs <- jsonlite::fromJSON(jsonlite::toJSON(out$data$locations, auto_unbox = TRU
   ) %>%
   dplyr::select(-cars) %>%
   tidyr::unnest_wider(car, names_sep = ".") %>%
-  # (optioneel) zorg dat booleans en numerics netjes zijn
   mutate(
     across(c(isRailway, isDiscount, isSpecialAccess, car.isInUse, car.hasKey, car.availability.available), as.logical),
-    across(c(geoPoint.lat, geoPoint.lng), as.numeric)
+    across(c(geoPoint.lat, geoPoint.lng), as.numeric),
+    date    = format(tz_now, "%Y-%m-%d"),
+    hour    = as.integer(format(tz_now, "%H")),
+    minute  = as.integer(format(tz_now, "%M")),
+    weekday = format(tz_now, "%A")
   )
 
 # Schrijf naar CSV met timestamp in bestandsnaam
